ce541cb3a92a0c95354cd002360456efaa457bdd
Evgeny <jenkneo.lal@gmail.com>
2024-10-26 16:43:04 +0400

    dev-1: linked to Postgres db

--- app/db/database.py
+++ app/db/database.py
@@ -8,7 +8,15 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+# Загружаем параметры подключения из env
+DATABASE_USERNAME = os.getenv('DATABASE_USERNAME')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+DATABASE_HOST = os.getenv('DATABASE_HOST')
+DATABASE_PORT = os.getenv('DATABASE_PORT')
+DATABASE_NAME = os.getenv('DATABASE_NAME')
+
+# Формируем URL для PostgreSQL
+DATABASE_URL = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
 engine = create_engine(
     DATABASE_URL,
