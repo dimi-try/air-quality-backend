455dfb3031f73067a885524d5a7ac773367da1c5
Dmitry <103138302+nuafirytiasewo@users.noreply.github.com>
2024-12-16 19:43:56 +0400

    hotfix крч вроде все работает. убрал удаление локации при удалении подписки. иначе никак не хочет работать система.

--- app/db/crud.py
+++ app/db/crud.py
@@ -1,4 +1,4 @@
-# crud.py
+# crud.py 
 from sqlalchemy.orm import Session
 from app.db.models import User, Subscription, Location, MapCache
 from sqlalchemy.exc import NoResultFound
@@ -7,31 +7,31 @@ import datetime
 from worker import force_update_database
 import logging
 
-
+# Создание или обновление подписки пользователя
 def create_or_update_subscription(
     db: Session, tg_user: dict, coordinates: dict, city: str, current_aqi: int
 ) -> Subscription:
-    # Проверяем, существует ли пользователь с указанным telegram_id
+    # Получаем пользователя по telegram_id
     user = db.query(User).filter(User.id == tg_user.id).first()
 
     if not user:
-        # Если пользователя нет, создаем его
+        # Если пользователь не найден, создаем нового
         user = User(
             id=tg_user.id, 
             username=tg_user.username, 
             first_name=tg_user.first_name, 
             last_name=tg_user.last_name
-            )
-        db.add(user)
-        db.commit()
-        db.refresh(user)
+        )
+        db.add(user)  # Добавляем пользователя в сессию
+        db.commit()  # Сохраняем изменения в базе данных
+        db.refresh(user)  # Обновляем объект user с реальными данными из БД
 
-    # Удаляем существующую подписку если она имеется
+    # Удаляем существующую подписку пользователя (если есть)
     subscription = db.query(Subscription).filter(Subscription.user_id == tg_user.id).first()
     if subscription:
         delete_subscription(db, tg_user.id)
 
-    # Проверяем, существует ли локация с указанными данными
+    # Проверяем наличие локации с указанными данными (город, координаты)
     location = (
         db.query(Location)
         .filter(
@@ -50,11 +50,11 @@ def create_or_update_subscription(
             aqi=current_aqi,
             created_by="telegram_user"
         )
-        db.add(location)
+        db.add(location)  # Добавляем новую локацию
         db.commit()
         db.refresh(location)
 
-    # Проверяем, существует ли подписка для данного пользователя и локации
+    # Проверяем наличие подписки для текущего пользователя и локации
     subscription = (
         db.query(Subscription)
         .filter(
@@ -64,36 +64,40 @@ def create_or_update_subscription(
     )
 
     if subscription:
-        # Если подписка существует, обновляем её
-        subscription.location.aqi = current_aqi  # Обновляем AQI локации
+        # Если подписка существует, обновляем значение AQI для локации
+        subscription.location.aqi = current_aqi
         db.commit()
         db.refresh(subscription)
         return subscription
 
-    # Если подписки нет, создаем новую
+    # Создаем новую подписку, если её не было
     new_subscription = Subscription(
         user_id=user.id, 
         location_id=location.id
-        )
+    )
     db.add(new_subscription)
     db.commit()
     db.refresh(new_subscription)
 
     return new_subscription
 
-
+# Получение всех пользователей
 def get_all_users(db: Session) -> list[Subscription]:
+    # Возвращаем список всех пользователей из таблицы User
     return db.query(User).all()
 
+# Добавление локаций из CSV-файла
 def add_locations_from_csv(db: Session, locations_data: list[list[str]]) -> int:
-    count = 0
+    count = 0  # Счетчик успешно добавленных локаций
     try:
-        for data in locations_data[1:]:
+        for data in locations_data[1:]:  # Пропускаем заголовок CSV
             city, longitude, latitude, radius = data
+            # Проверяем, существует ли локация с таким городом
             location_exists = db.query(Location).filter(Location.city == city).first()
             if location_exists:
-                logging.info(f"Город {city} уже существует в таблице Map")
+                logging.info(f"Город {city} уже существует в таблице Location")
                 continue
+            # Создаем новую локацию
             location = Location(
                 city=city,
                 longitude=float(longitude),
@@ -101,17 +105,18 @@ def add_locations_from_csv(db: Session, locations_data: list[list[str]]) -> int:
                 radius=int(radius.rstrip('\r')),
                 created_by="default"
             )
-            db.add(location)
+            db.add(location)  # Добавляем локацию
             count += 1
-        db.commit()
+        db.commit()  # Сохраняем изменения
         return count
     except Exception as e:
-        logging.error(f"Произошла ошибка при добавлении данных в таблицу map: {e}")
+        logging.error(f"Ошибка при добавлении данных в таблицу Location: {e}")
         return 0
-    force_update_database()
+    force_update_database()  # Принудительное обновление базы данных
 
-
+# Обновление значения AQI для указанной локации
 def update_location_aqi(db: Session, coordinates: dict, current_aqi: int) -> Subscription:
+    # Получаем локацию по координатам
     location = (
         db.query(Location)
         .filter(
@@ -119,45 +124,46 @@ def update_location_aqi(db: Session, coordinates: dict, current_aqi: int) -> Sub
             Location.latitude == coordinates['lat'])
         .first()
     )
+    # Обновляем значение AQI
     location.aqi = current_aqi
     db.commit()
     db.refresh(location)
     return location
 
-# Delete
+# Удаление подписки пользователя
 def delete_subscription(db: Session, telegram_id: int) -> bool:
+    # Получаем подписку пользователя по telegram_id
     subscription = db.query(Subscription).filter(Subscription.user_id == telegram_id).first()
     if not subscription:
-        return False
-
-    location = db.query(Location).filter(Location.id == subscription.location_id).first()
-
-    if location.created_by == "telegram_user":
-        db.query(MapCache).filter(MapCache.location_id == location.id).delete()
-        db.delete(location)
-    db.delete(subscription)
+        return False  # Подписка не найдена
+    
+    db.delete(subscription)  # Удаляем подписку
     db.commit()
     return True
 
-
 # Получение подписки по telegram_id
 def get_subscription_by_telegram_id(db: Session, telegram_id: int) -> User:
+    # Получаем пользователя по telegram_id
     user = db.query(User).filter(User.id == telegram_id).first()    
     if not user:
         return None
 
-    # Обновляем объект, чтобы он снова был привязан к сессии
-    db.refresh(user)  # Это позволяет убедиться, что атрибуты объекта доступны
-
+    # Обновляем объект пользователя
+    db.refresh(user)  
     return user
 
+# Получение всех локаций
 def get_all_locations(db: Session) -> list[Location]:
+    # Возвращаем все записи из таблицы Location
     return db.query(Location).all()
 
+# Обновление данных кэша карты
 def update_map_cache(db: Session, location_info: dict, location: Location) -> Location:
+    # Проверяем наличие записи в MapCache для указанной локации
     map_cache = db.query(MapCache).filter(MapCache.location_id == location.id).first()
 
     if not map_cache:
+        # Если записи нет, создаем новую
         map_cache = MapCache(
             location_id=location.id,
             expiration_date=datetime.datetime.utcfromtimestamp(location_info['list'][0]['dt'] + MAP_DATA_TTL),
@@ -170,11 +176,12 @@ def update_map_cache(db: Session, location_info: dict, location: Location) -> Lo
             pm10=location_info['list'][0]['components']['pm10'],
             nh3=location_info['list'][0]['components']['nh3']
         )
-        db.add(map_cache)
+        db.add(map_cache)  # Добавляем запись в кэш
         db.commit()
         db.refresh(map_cache)
         return location
 
+    # Обновляем данные в существующей записи MapCache
     map_cache.expiration_date = datetime.datetime.utcfromtimestamp(location_info['list'][0]['dt'] + MAP_DATA_TTL)
     map_cache.co =              location_info['list'][0]['components']['co']
     map_cache.no =              location_info['list'][0]['components']['no']
@@ -189,5 +196,7 @@ def update_map_cache(db: Session, location_info: dict, location: Location) -> Lo
     db.refresh(location)
     return location
 
+# Получение данных кэша карты
 def get_map_cache(db: Session) -> list[Location]:
+    # Возвращаем все записи из таблицы MapCache
     return db.query(MapCache).all()
